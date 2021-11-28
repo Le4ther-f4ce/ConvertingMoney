c2f100cc79a20b26d02addb2571d0843bffe0659
Алексей Федюшин <alexeyfedyshinural@gmail.com>
2021-11-28 17:09:19 +0500

    Добавьте файлы проекта.

---
+++ ConvertingMoney/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConvertingMoney
+{
+    class Program
+    {
+        /*
+         * При старте программы пользователь вводит начальное количество золота.
+         * Потом ему предлагается купить какое-то количество 
+         * кристаллов по цене N(задать в программе самому). 
+         * Пользователь вводит число и его золото конвертируется в кристаллы.
+         * Остаток золота и кристаллов выводится на экран.
+         */
+        static void Main(string[] args)
+        {
+            int priceCrystal = 100;
+
+            Console.Write("Введите количество ваших золотых: ");
+            int usersGold = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Цена одного кристала - {priceCrystal},\n" +
+                $"сколько кристалов приобрести: ");
+            int purchasedCrystals = Convert.ToInt32(Console.ReadLine());
+
+            usersGold -= (purchasedCrystals * priceCrystal);
+            Console.WriteLine($"У вас {usersGold}" +
+                $" золотых и {purchasedCrystals} кристалов");
+        }
+    }
+}
